194845212377c7cd7f8e0914300fd67f59ce9588
Lyj <240754337@qq.com>
2019-08-13 16:37:12 +0800

    新建一个vue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-
 Vue.config.productionTip = false
+
+//全局样式
+import './assets/css/base.less'
+
+//饿了么ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+//use一下
+Vue.use(ElementUI)
 
 new Vue({
   render: h => h(App),
